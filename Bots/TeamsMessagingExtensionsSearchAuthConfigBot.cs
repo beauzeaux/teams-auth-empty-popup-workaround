c64ae5d570f2518df9688b9f7897730a4edce514
Nick Beaulieu <nibeauli@microsoft.com>
2020-11-16 15:03:54 -0800

    Initial commit

---
+++ Bots/TeamsMessagingExtensionsSearchAuthConfigBot.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Teams;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Schema.Teams;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.BotBuilderSamples.Bots
+{
+    public class TeamsMessagingExtensionsSearchAuthConfigBot : TeamsActivityHandler
+    {
+        readonly string _connectionName;
+
+        public TeamsMessagingExtensionsSearchAuthConfigBot(IConfiguration configuration)
+        {
+            _connectionName = configuration["ConnectionName"] ?? throw new NullReferenceException("ConnectionName");
+        }
+
+        public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
+        {
+            await base.OnTurnAsync(turnContext, cancellationToken);
+        }
+
+        protected override async Task<MessagingExtensionActionResponse> OnTeamsMessagingExtensionSubmitActionAsync(ITurnContext<IInvokeActivity> turnContext, MessagingExtensionAction action, CancellationToken cancellationToken)
+        {
+            if (action.CommandId.ToUpper() == "COMMAND") {
+                var signInLink = await (turnContext.Adapter as IUserTokenProvider).GetOauthSignInLinkAsync(turnContext, _connectionName, cancellationToken);
+                return new MessagingExtensionActionResponse
+                {
+                    ComposeExtension = new MessagingExtensionResult
+                    {
+                        Type = "auth",
+                        SuggestedActions = new MessagingExtensionSuggestedAction
+                        {
+                            Actions = new List<CardAction>
+                            {
+                                new CardAction
+                                {
+                                    Type = ActionTypes.OpenUrl,
+                                    Value = signInLink,
+                                    Title ="Sign In"
+                                },
+                            },
+                        },
+                    },
+                };
+            }
+            return new MessagingExtensionActionResponse();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+﻿# Teams Interstitial auth fix
+
+This is a quick fix for the intermediate blank popup that shows during auth flows. 
+
+This works by first loading a website controlled by the developer into a task module that then triggers the auth flow
+via an sdk call `microsoftTeams.tasks.submitTask`
+
+The relevant changes can be found in TeamsAppManifest/manifest.json (note the command w/o parameters and a task info url) and in the `authStart` page inside wwwroot
